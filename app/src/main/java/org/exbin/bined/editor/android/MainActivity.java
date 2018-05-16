c3cc337c9c10e6ea7f6db3daf55c1563a9a4ce6d
hajdam <hajdam@users.sf.net>
2018-05-16 22:12:43 +0200

    Added basic open file action

--- app/src/main/java/org/exbin/bined/editor/android/MainActivity.java
+++ app/src/main/java/org/exbin/bined/editor/android/MainActivity.java
@@ -1,26 +1,43 @@
 package org.exbin.bined.editor.android;
 
+import android.Manifest;
+import android.app.DialogFragment;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.rustamg.filedialogs.FileDialog;
+import com.rustamg.filedialogs.OpenFileDialog;
+import com.rustamg.filedialogs.SaveFileDialog;
 
 import org.exbin.bined.android.CodeArea;
 import org.exbin.utils.binary_data.ByteArrayEditableData;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FileDialog.OnFileSelectedListener {
 
     private static final String EXAMPLE_FILE_PATH = "/org/exbin/bined/android/example/resources/lorem_1.txt";
+
+    private CodeArea codeArea;
+
+    private boolean storagePermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.storagePermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -33,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-        CodeArea codeArea = findViewById(R.id.codeArea);
+        codeArea = findViewById(R.id.codeArea);
 
         ByteArrayEditableData basicData = new ByteArrayEditableData();
         try {
@@ -60,6 +77,36 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         switch (item.getItemId()) {
+            case R.id.action_open: {
+                if (storagePermissionGranted) {
+                    OpenFileDialog dialog = new OpenFileDialog();
+                    dialog.setStyle(DialogFragment.STYLE_NO_TITLE, R.style.AppTheme);
+                    dialog.show(getSupportFragmentManager(), OpenFileDialog.class.getName());
+                } else {
+                    showPermissionError();
+                }
+
+                return true;
+            }
+
+            case R.id.action_save: {
+                if (storagePermissionGranted) {
+                    FileDialog dialog = new SaveFileDialog();
+                    dialog.setStyle(DialogFragment.STYLE_NO_TITLE, R.style.AppTheme);
+                    dialog.show(getSupportFragmentManager(), SaveFileDialog.class.getName());
+                } else {
+                    showPermissionError();
+                }
+
+                return true;
+            }
+
+            case R.id.action_exit: {
+                // TODO
+                System.exit(0);
+                return true;
+            }
+
             case R.id.action_settings:
                 // User chose the "Settings" item, show the app settings UI...
                 return true;
@@ -71,6 +118,28 @@ public class MainActivity extends AppCompatActivity {
 
             default:
                 return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void showPermissionError() {
+        Toast.makeText(this, "Storage permission is not granted", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public void onFileSelected(FileDialog dialog, File file) {
+        if (dialog instanceof OpenFileDialog) {
+            ByteArrayEditableData fileData = new ByteArrayEditableData();
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                fileData.loadFromStream(fileInputStream);
+            } catch (IOException ex) {
+                Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            codeArea.setContentData(fileData);
+        } else {
 
         }
     }
