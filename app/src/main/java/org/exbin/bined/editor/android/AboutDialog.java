94db72c1dc4adb6285a7bc1636ac48fe80212586
hajdam <hajdam@users.sf.net>
2021-04-15 00:02:22 +0200

    Added about dialog

--- app/src/main/java/org/exbin/bined/editor/android/AboutDialog.java
+++ app/src/main/java/org/exbin/bined/editor/android/AboutDialog.java
@@ -4,15 +4,25 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class AboutDialog extends AppCompatDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        FragmentActivity activity = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setTitle("About App");
-        builder.setView(getActivity().findViewById(R.id.aboutView));
+        // Get the layout inflater
+        LayoutInflater inflater = activity.getLayoutInflater();
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the
+        // dialog layout
+        View aboutView = inflater.inflate(R.layout.about_view, null);
+        builder.setView(aboutView);
         builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
